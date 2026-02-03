9988e0e8fd3ed931629e4873e662abee415c47a9
Nikolai Palashev <nikolai.palashev@gmail.com>
2026-02-03 10:32:12 +0200

    Add project files.

---
+++ BuditelWebServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BuditelWebServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
